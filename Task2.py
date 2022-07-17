a2f410866d3b4e2acbdf63bd48b19fc84d208624
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-07-18 00:51:01 +0300

    task2 completed

--- Task2.py
+++ Task2.py
@@ -1,8 +1,18 @@
-# 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N. (факториалы чисел)
 #     Пример:
 #     - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-
+number = int(input("Введите число N ->")) 
+ 
+def subseq_factorial(numm): 
+    subseq=[1] 
+    factorial=1 
+    for i in range(2,numm+1): 
+        factorial=i*factorial 
+        subseq.append(factorial) 
+    return subseq 
+ 
+print(subseq_factorial(number))
 
 
 
